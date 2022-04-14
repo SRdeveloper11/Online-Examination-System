d5885bd4574704179b5dc536de0038483dfaeec2
SnehaBasak123 <101534960+SnehaBasak123@users.noreply.github.com>
2022-04-14 18:37:02 +0530

    h

--- ExamPortal/src/app/components/nav-bar/nav-bar.component.ts
+++ ExamPortal/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginService } from "src/app/services/login.service";
 
 @Component({
@@ -10,7 +11,7 @@ export class NavBarComponent implements OnInit {
 
   public loggedIn= false;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,public router:Router) { }
 
   ngOnInit(): void {
     this.loggedIn = this.loginService.isLogedIn();
