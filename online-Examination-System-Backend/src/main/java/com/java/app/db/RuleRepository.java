f7d90f81ff8cf9b8fb28b1033af48cb0ee6e1f73
pranav99d <pranavdere99@gmail.com>
2022-04-26 18:37:31 +0530

    Updating

--- online-Examination-System-Backend/src/main/java/com/java/app/db/RuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.java.app.db;
-import com.java.app.model.Rule;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface RuleRepository extends JpaRepository<Rule,Integer> {
-	Rule getByName(String name);
-
-	Rule findByName(String name);
-}
